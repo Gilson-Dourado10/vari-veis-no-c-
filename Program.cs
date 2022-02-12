679bef8151afb2630fb34b801fc0c99e4652f11e
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com.br>
2022-02-12 06:11:25 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 25; 
+            var texto = "testando";
+            Console.WriteLine( idade);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(texto);
+        
+        }
+    }
+}
